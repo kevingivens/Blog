a2b2d3ce3164fee90be6c47b9c6f177d3020f4c9
Kevin Givens <givenskevinm@gmail.com>
2024-01-14 00:26:19 -0500

    rename files

--- 2024/CythonFunctional/setup.py
+++ 2024/CythonFunctional/setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
-setup(name="funclib",
-      ext_modules=cythonize(
-          ["py_func.pyx"], 
-          language_level = "3",
-      )
+#setup(name="funclib",
+#      ext_modules=cythonize(
+#          ["py_func.pyx"], 
+#          language_level = "3",
+#      )
+#      extra_compile_arg
+#)
+
+from Cython.Distutils import build_ext
+
+setup(
+  name = 'Test app',
+  ext_modules=[
+    Extension('cpp_function_lib',
+              sources=['cpp_function_lib.pyx'],
+              extra_compile_args=['-std=c++11'],
+              language='c++')
+    ],
+   cmdclass = {'build_ext': build_ext}
 )
